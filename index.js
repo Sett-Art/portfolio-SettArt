083e531c5c90ad1dd0b3fba46af9a8bcf7635bc4
Sett-Art <165786994+Sett-Art@users.noreply.github.com>
2024-04-02 13:14:46 +0000

    Modification

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
-  /*carroussel*/
-  let slideIndex = 0;
+/*carroussel*/
+let slideIndex = 0;
 
 function moveCarousel(n) {
-  const slides = document.querySelectorAll('.carousel-item');
-  slideIndex += n;
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
-  } else if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  }
-  const slideWidth = slides[0].clientWidth;
-  const offset = -slideWidth * slideIndex;
-  document.querySelector('.carousel-inner').style.transform = `translateX(${offset}px)`;
+const slides = document.querySelectorAll('.carousel-item');
+slideIndex += n;
+if (slideIndex >= slides.length) {
+  slideIndex = 0;
+} else if (slideIndex < 0) {
+  slideIndex = slides.length - 1;
+}
+const slideWidth = slides[0].clientWidth;
+const offset = -slideWidth * slideIndex;
+document.querySelector('.carousel-inner').style.transform = `translateX(${offset}px)`;
 }
 
